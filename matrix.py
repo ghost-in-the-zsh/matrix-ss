a88992fe15bde81d4a72e4524050daaf3c539b23
Raymond L. Rivera <ray.l.rivera@gmail.com>
2022-12-24 02:00:56 -0400

    Add stream trails and refactor code
    Signed-off-by: Raymond L. Rivera <ray.l.rivera@gmail.com>

--- matrix.py
+++ matrix.py
@@ -1,15 +1,21 @@
+import sys
+
 import pygame as pg
-import pygame.freetype
+import pygame.freetype as ft
 import numpy as np
 
 from stream import Stream
 
 class Matrix:
-    FPS = 60
-    MIN_DELAY = 1
-    MAX_DELAY = 256
+    FPS = 30
+    MIN_MSECS = 1
+    MAX_MSECS = 256
     FONT_SIZE = 8
     BGCOLOR = pg.Color('black')
+    KATAKANA = [chr(int('0x30a0', 16) + c) for c in range(96)]
+    MIN_COLOR = 100
+    MAX_COLOR = 255
+    DELTA_COLOR = 5
 
     def __init__(self):
         pg.init()
@@ -17,43 +23,48 @@ class Matrix:
         size = (info.current_w, info.current_h)
         self.screen = pg.display.set_mode(size)
         self.surface = pg.Surface(size)
-        self.font = pg.freetype.Font('font/msmincho.ttf', Matrix.FONT_SIZE)
+        self.font = ft.Font('font/msmincho.ttf', Matrix.FONT_SIZE)
         self.clock = pg.time.Clock()
         self.streams = []
-        self.stream_delays = [i for i in range(Matrix.MIN_DELAY, Matrix.MAX_DELAY)]
-        self._setup_streams()
+        self.stream_delays = [i for i in range(Matrix.MIN_MSECS, Matrix.MAX_MSECS)]
+        self.setup_streams()
         self.elapsed_msecs = pg.time.get_ticks()
         pg.display.toggle_fullscreen()
         pg.mouse.set_visible(False)
-
-    def _setup_streams(self):
-        stream_cnt = self.screen.get_width() // Matrix.FONT_SIZE
-        stream_len = self.screen.get_height() // Matrix.FONT_SIZE + 1
-        [self.streams.append(Stream(self, col, stream_len)) for col in range(stream_cnt)]
-        [self.on_stream_delay_update(s) for s in self.streams]
-
-    def _update(self, delta_msecs: float):
-        self.surface.fill(Matrix.BGCOLOR)
-        [s.update(delta_msecs) for s in self.streams]
-        self.screen.blit(self.surface, (0, 0))
 
     def run(self) -> None:
         while True:
             current_msecs = pg.time.get_ticks()
             delta_msecs = current_msecs - self.elapsed_msecs
-            self._update(delta_msecs)
-            [self._on_event(e) for e in pg.event.get()]
+            self.update(delta_msecs)
+            [self.on_event(e) for e in pg.event.get()]
             pg.display.flip()
             self.clock.tick(Matrix.FPS)
             self.elapsed_msecs = current_msecs
 
+    def add_stream(self, column: int, length: int) -> Stream:
+        s = Stream(self, column, length)
+        self.streams.append(s)
+        return s
+
+    def setup_streams(self):
+        stream_cnt = self.screen.get_width() // Matrix.FONT_SIZE
+        stream_len = self.screen.get_height() // Matrix.FONT_SIZE + 1
+        [self.add_stream(col, stream_len) for col in range(stream_cnt)]
+        [self.on_stream_delay_update(s) for s in self.streams]
+
+    def update(self, delta_msecs: float):
+        self.surface.fill(Matrix.BGCOLOR)
+        [s.update(delta_msecs) for s in self.streams]
+        self.screen.blit(self.surface, (0, 0))
+
     def on_stream_delay_update(self, stream: Stream):
         stream.delay_msecs = np.random.choice(self.stream_delays)
 
-    def _on_event(self, event):
+    def on_event(self, event):
         if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
-            self._exit()
+            self.exit()
 
-    def _exit(self):
+    def exit(self):
         pg.quit()
-        raise SystemExit
+        sys.exit(0)
--- stream.py
+++ stream.py
@@ -1,25 +1,29 @@
 import pygame as pg
-import pygame.freetype
 import numpy as np
 
 class Stream:
-    KATAKANA = [chr(int('0x30a0', 16) + c) for c in range(96)]
-
     def __init__(self, matrix, col_idx: int, len: int) -> None:
         self.app = matrix
         self.column = col_idx
         self.chars = [''] * len
+        # prevent the same color ref in all list entries
+        self.colors = []
+        for _ in range(len):
+            self.colors.append(pg.Color(0, matrix.MAX_COLOR, 0))
         self.delay_msecs = 0
         self._char_idx = 0
 
-    def update(self, delta_msecs: float):
+    def update(self, delta_msecs: int):
         if self.delay_msecs <= 0:
-            self.chars[self._char_idx] = np.random.choice(Stream.KATAKANA)
+            idx = self._char_idx
+            self.chars[idx] = np.random.choice(self.app.KATAKANA)
+            self.colors[idx].g = self.app.MAX_COLOR
             self._char_idx = (self._char_idx + 1) % len(self.chars)
             self.app.on_stream_delay_update(self)
 
         for row, char in enumerate(self.chars):
             pos = (self.column * self.app.FONT_SIZE, row * self.app.FONT_SIZE)
-            self.app.font.render_to(self.app.surface, pos, char, fgcolor=(0, 220, 0))
+            self.app.font.render_to(self.app.surface, pos, char, fgcolor=self.colors[row])
+            self.colors[row].g = max(self.colors[row].g - self.app.DELTA_COLOR, self.app.MIN_COLOR)
 
         self.delay_msecs -= delta_msecs
