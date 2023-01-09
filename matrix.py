0a5c138b03276e8d52d78df4cb78f488841a3ff1
Raymond L. Rivera <ray.l.rivera@gmail.com>
2023-01-08 22:37:50 -0400

    Rename methods
    Just applying some PEP-8.
    
    Signed-off-by: Raymond L. Rivera <ray.l.rivera@gmail.com>

--- matrix.py
+++ matrix.py
@@ -33,8 +33,8 @@ class Matrix:
         self.surface = pg.Surface(size)
         self.font = ft.Font('font/msmincho.ttf', Matrix.FONT_SIZE)
         self.clock = pg.time.Clock()
-        self.wallpaper = self.get_wallpaper()
-        self.setup_streams()
+        self.wallpaper = self._get_wallpaper()
+        self._setup_streams()
         self.elapsed_msecs = pg.time.get_ticks()
         if Matrix.FULLSCREEN:
             pg.display.toggle_fullscreen()
@@ -44,30 +44,30 @@ class Matrix:
         while True:
             current_msecs = pg.time.get_ticks()
             delta_msecs = current_msecs - self.elapsed_msecs
-            self.update(delta_msecs)
+            self._update(delta_msecs)
             for e in pg.event.get():
-                self.handle_event(e)
+                self._handle_event(e)
             pg.display.flip()
             self.clock.tick(Matrix.FPS)
             self.elapsed_msecs = current_msecs
 
-    def setup_streams(self):
+    def _setup_streams(self):
         stream_cnt = self.screen.get_width() // Matrix.FONT_SIZE
         stream_len = self.screen.get_height() // Matrix.FONT_SIZE
         self.streams = [Stream(self, col, stream_len) for col in range(stream_cnt)]
 
-    def update(self, delta_msecs: float):
+    def _update(self, delta_msecs: float):
         self.surface.fill(Matrix.BGCOLOR)
         for s in self.streams: s.update(delta_msecs)
         self.screen.blit(self.surface, (0, 0))
         if not self.FULLSCREEN:
             pg.display.set_caption('FPS: {:.2f}'.format(self.clock.get_fps()))
 
-    def handle_event(self, event) -> None:
+    def _handle_event(self, event) -> None:
         if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
-            self.exit()
+            self._exit()
 
-    def get_wallpaper(self) -> pg.pixelarray.PixelArray:
+    def _get_wallpaper(self) -> pg.pixelarray.PixelArray:
         sys = pt.system()
         if sys == 'Linux':
             # TODO: check distro, gnome/kde, subprocess command to get wallpaper
@@ -83,6 +83,6 @@ class Matrix:
         image = pg.transform.scale(image, self.screen.get_size()).convert()
         return pg.pixelarray.PixelArray(image)
 
-    def exit(self):
+    def _exit(self):
         pg.quit()
         sys.exit(0)
--- stream.py
+++ stream.py
@@ -3,7 +3,6 @@ from random import randrange, choice
 import pygame as pg
 from pygame import Color
 
-
 class Char:
     def __init__(
         self,
@@ -16,17 +15,17 @@ class Char:
         self.limit = limit if limit else Color('black')
 
 class Stream:
-    def __init__(self, matrix, col_idx: int, len: int) -> None:
+    def __init__(self, matrix, col_idx: int, length: int) -> None:
         self.app = matrix
         self.column = col_idx
-        self.chars = [Char() for _ in range(len)]
+        self.chars = [Char() for _ in range(length)]
         updates_limit = randrange(1, 5)
         self.char_delays = [
             randrange(matrix.MIN_MSECS, matrix.MAX_MSECS)
             for _ in range(updates_limit)
         ]
         self.char_indices = list(set([
-            randrange(0, len)
+            randrange(0, length)
             for _ in range(updates_limit)
         ]))
 
@@ -34,12 +33,12 @@ class Stream:
         for i in range(len(self.char_indices)):
             self.char_delays[i] -= delta_msecs
             if self.char_delays[i] <= 0:
-                self.change_char(self.char_indices[i])
+                self._change_char(self.char_indices[i])
                 self.char_indices[i] = (self.char_indices[i] + 1) % len(self.chars)
                 self.char_delays[i] = randrange(self.app.MIN_MSECS, self.app.MAX_MSECS)
-        self.render()
+        self._render()
 
-    def change_char(self, idx: int) -> None:
+    def _change_char(self, idx: int) -> None:
         pos = (self.column * self.app.FONT_SIZE, idx * self.app.FONT_SIZE)
         ch = self.chars[idx]
         ch.glyph = choice(self.app.KATAKANA)
@@ -51,7 +50,7 @@ class Stream:
         ch.color.g = min(round(ch.color.g * self.app.MAX_FACTOR), self.app.MAX_COLOR)
         ch.color.b = min(round(ch.color.b * self.app.MAX_FACTOR), self.app.MAX_COLOR)
 
-    def render(self) -> None:
+    def _render(self) -> None:
         for row, char in enumerate(filter(lambda ch: ch.glyph, self.chars)):
             pos = (self.column * self.app.FONT_SIZE, row * self.app.FONT_SIZE)
             ch = self.chars[row]
