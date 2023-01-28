82cd5150fb26380dfa200b4a29586e2897f6ff15
Raymond L. Rivera <ray.l.rivera@gmail.com>
2023-01-28 04:14:55 -0700

    stream: Add docs and refactor some code
    The refactoring was renaming a var and using a set-comprehension instead
    of a list-comprehension that is then passed onto a set.
    
    Signed-off-by: Raymond L. Rivera <ray.l.rivera@gmail.com>

--- stream.py
+++ stream.py
@@ -1,15 +1,22 @@
+'''The stream module.
+
+A stream is a single column of text on the screen. Each stream has one or
+more "trails". A trail is defined by the character being drawn within the
+stream, being brightest when drawn and dimming out with time.
+'''
+
 from dataclasses import dataclass
 from random import randrange, choice
 
-import pygame as pg
 from pygame import Color
 
 class _Char:
+    '''Character data including glyph and colors.'''
     def __init__(
         self,
-        glyph: str=None,
-        color: Color=None,
-        limit: Color=None
+        glyph: str=None,    # the symbol to render
+        color: Color=None,  # color to use when rendering the glyph
+        limit: Color=None   # lower bound on how dim to make the rendering color
     ) -> None:
         self.glyph = glyph
         self.color = color if color else Color('black')
@@ -17,8 +24,8 @@ class _Char:
 
 @dataclass
 class _Time:
-    delay: int=0    # in milliseconds
-    accum: int=0    # in milliseconds
+    delay: int=0    # how long to wait before rendering a char, in msec
+    accum: int=0    # how long has passed since we started waiting, in msec
 
     def update(self, msecs: int) -> None:
         self.accum += msecs
@@ -32,19 +39,23 @@ class _Time:
 
 @dataclass
 class _Trail:
-    idx: int=0  # stream index
+    idx: int=0  # "head" index within stream
     time: _Time=None
 
 class Stream:
     def __init__(self, matrix, col_idx: int, length: int) -> None:
+        # _column: screen column index
+        # _chars : list of characters to be rendered
+        # _trails: list of stream indices where trail "heads" are to be rendered
+        # length : how long this stream column is (in chars)
         self._app = matrix
         self._column = col_idx
         self._chars = [_Char() for _ in range(length)]
         max_trails = randrange(1, 3)
-        trail_indices = list(set([randrange(0, length) for _ in range(max_trails)]))
-        trail_count = len(trail_indices)
+        stream_indices = list({randrange(0, length) for _ in range(max_trails)})
+        trail_count = len(stream_indices)
         self._trails = [_Trail(
-            trail_indices[i],
+            stream_indices[i],
             _Time(randrange(matrix.MIN_MSECS, matrix.MAX_MSECS))
         ) for i in range(trail_count)]
 
